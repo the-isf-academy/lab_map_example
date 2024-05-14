983df605b6217e6972b25b82c2b0f64f0866f67d
Emma <eqbrown@isf.edu.hk>
2024-05-14 12:19:14 +0800

    updates for spring 24, variables to properties, minor map changes, quality of life updates

--- game_platformer.py
+++ game_platformer.py
@@ -1,38 +1,9 @@
-########################################################
-# adapted from Python Aracde Example Games
-#
+############################################
 # 💻 Experiment with the settings, see what you discover!
-########################################################
+############################################
 
 
 import arcade
-
-
-# 💻 Try changing these numbers to adjust the scale of the game
-TILE_SCALING = 1
-PLAYER_SCALING = 1
-
-# Grid Size
-SPRITE_PIXEL_SIZE = 28
-GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
-
-# Screen Settings
-SCREEN_WIDTH = 200
-SCREEN_HEIGHT = 200
-SCREEN_TITLE = "Map Making"
-
-# How many pixels to keep as a minimum margin between the character
-# and the edge of the screen.
-VIEWPORT_MARGIN_TOP = 60
-VIEWPORT_MARGIN_BOTTOM = 20
-VIEWPORT_RIGHT_MARGIN = 60
-VIEWPORT_LEFT_MARGIN = 60
-
-# Physics
-MOVEMENT_SPEED = 5
-JUMP_SPEED = 20
-GRAVITY = 1.1
-
 
 class MyGame(arcade.Window):
     """Main application class."""
@@ -41,10 +12,23 @@ class MyGame(arcade.Window):
         """
         Initializer
         """
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+        self.screen_width = 800
+        self.screen_height = 600
+
+        super().__init__(
+            width = self.screen_width, 
+            height = self.screen_height, 
+            title = "Game 1")
+
+
+        # Physics
+        self.MOVEMENT_SPEED = 5
+        self.JUMP_SPEED = 25
+        self.GRAVITY = 1.1
+
+        # game over
         self.game_over = False
-        self.score = 0
         
      
         """Set up the game and initialize the variables."""
@@ -54,15 +38,22 @@ class MyGame(arcade.Window):
 
         self.player_sprite = arcade.Sprite(
             "assets/sprites/slime.png",
-            PLAYER_SCALING,
+            scale = 2,
+            center_x = 250,
+            center_y = 270,
         )
 
-        self.player_sprite.center_x = 250
-        self.player_sprite.center_y = 270
+        # loads another version of the sprite image, flipped 
+        texture = arcade.load_texture("assets/sprites/slime.png",
+                                      flipped_horizontally=True)
+        
+        self.player_sprite.textures.append(texture)
+
+    
         self.player_list.append(self.player_sprite)
 
         # sets up map
-        map_name = "assets/map/forest_map.tmj"
+        map_name = "assets/map/forest_map.tmj" 
 
         layer_options = {
             "Walls": {"use_spatial_hash": True},
@@ -70,34 +61,44 @@ class MyGame(arcade.Window):
             "End": {"use_spatial_hash": True}
         }
 
+        # map scaling
+        self.TILE_SCALING = 2
+        self.SPRITE_PIXEL_SIZE = 128
+        self.GRID_PIXEL_SIZE = self.SPRITE_PIXEL_SIZE * self.TILE_SCALING
+
         # read in the tiled map
         self.tile_map = arcade.load_tilemap(
-            map_name, layer_options=layer_options, scaling=TILE_SCALING
+            map_name, 
+            layer_options=layer_options,
+              scaling=self.TILE_SCALING
         )
 
-        self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
+        self.end_of_map = self.tile_map.width * self.GRID_PIXEL_SIZE
 
         # sets wall and coin SpriteLists
         self.wall_list = self.tile_map.sprite_lists["Walls"]
         self.coin_list = self.tile_map.sprite_lists["Coins"]
         self.end_list = self.tile_map.sprite_lists["End"]
         self.background_list = self.tile_map.sprite_lists["Background"]
-
   
         # sets the background color
         arcade.set_background_color(arcade.color.BLIZZARD_BLUE)
 
         # Keep player from running through the wall_list layer
-        walls = [self.wall_list, ]
+        walls = [self.wall_list]
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player_sprite, walls, gravity_constant=GRAVITY
+            self.player_sprite, walls, gravity_constant=self.GRAVITY
         )
 
         # sets up camera 
-        self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.camera = arcade.Camera(self.screen_width, self.screen_height)
 
         # center camera on user
         self.pan_camera_to_user()
+
+        # creates score
+        self.score = 0
 
 
     def on_draw(self):
@@ -105,41 +106,61 @@ class MyGame(arcade.Window):
         Render the screen.
         """
 
-        # These commands must happen before we start drawing
-        self.camera.use()
-        self.clear()
+        # if game is not over, draw everything
 
-        # draw all the map
-        self.background_list.draw()
-        self.end_list.draw()
-        self.wall_list.draw()
-        self.coin_list.draw()
+        if self.game_over == False:
+            self.camera.use()
+            self.clear()
 
-        # draws the sprites
-        self.player_list.draw()
+            # draws the player
+            self.player_list.draw()
+            
+            # draws the map layers
+            self.background_list.draw()
+            self.wall_list.draw()
+            self.coin_list.draw()
+            self.end_list.draw()
 
-        if self.game_over == True:
-            x_pos = self.player_sprite.center_x+100
-            y_pos = self.player_sprite.center_y+100
-            text_size = 25
+            # draws the score
+            arcade.draw_text(
+                f"Score: {self.score}",
+                self.camera.position[0] + 25,  
+                self.camera.position[1]+ 550,  
+                arcade.color.BLACK,
+                20,  
+        )
+  
+        # else, the game is over - only draw text
+        else :
+            self.clear()
 
-            arcade.draw_text(f"Score: {self.score}", x_pos, y_pos, arcade.color.WHITE, text_size)
-    
+            arcade.draw_text(
+                f"Game Over",
+                self.camera.position[0]+ self.screen_width/3,
+                self.camera.position[1]+ self.screen_height/2,
+                arcade.color.BLACK,
+                40
+            )
 
     def on_key_press(self, key, modifiers):
         """
         Called whenever a key is pressed.
         """
 
-        if key == arcade.key.UP:
-            if self.physics_engine.can_jump():
-                self.player_sprite.change_y = JUMP_SPEED
+        if key == arcade.key.W:
+            self.player_sprite.change_y = self.JUMP_SPEED
 
-        elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.A:
+            self.player_sprite.change_x = -self.MOVEMENT_SPEED
 
-        elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = MOVEMENT_SPEED
+            # switches the current player's sprite texture to face to the left
+            self.player_sprite.texture = self.player_sprite.textures[1]
+
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = self.MOVEMENT_SPEED
+
+            # switches the current player's sprite texture to face to the right
+            self.player_sprite.texture = self.player_sprite.textures[0]
 
         elif key == arcade.key.ESCAPE:
             arcade.exit()
@@ -149,17 +170,19 @@ class MyGame(arcade.Window):
         Called when the user presses a mouse button.
         """
         
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+        if key == arcade.key.A or key == arcade.key.D:
             self.player_sprite.change_x = 0
 
     def on_update(self, delta_time):
         """Movement and game logic"""
 
+        # prevent the player from falling off the left side of the map
+        if self.player_sprite.center_x < 0:
+            self.player_sprite.center_x = 0
+
+
         # if player hits the 'End' map layer, end game
-        if arcade.check_for_collision_with_list(
-            self.player_sprite, 
-            self.end_list
-        ):
+        if arcade.check_for_collision_with_list(self.player_sprite, self.end_list):
             self.game_over = True
 
         # Call update on all sprites
@@ -167,33 +190,30 @@ class MyGame(arcade.Window):
             self.physics_engine.update()
 
 
-        # if player collides with a coin
+        # manages if a player hits a coin
         coins_hit = arcade.check_for_collision_with_list(
-            self.player_sprite, 
-            self.coin_list
+            self.player_sprite, self.coin_list
         )
 
         for coin in coins_hit:
             coin.remove_from_sprite_lists()
             self.score += 1
-           
 
-        # pans camera to the user
+        # Pan to the user
         self.pan_camera_to_user(panning_fraction=0.12)
 
     def pan_camera_to_user(self, panning_fraction: float = 1.0):
         """ Manage Scrolling """
 
         # This spot would center on the user
-        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
-        screen_center_y = self.player_sprite.center_y - (
-            self.camera.viewport_height / 3
-        )
-        
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width/2)
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height/3)
+
         if screen_center_x < 0:
             screen_center_x = 0
         if screen_center_y < 0:
             screen_center_y = 0
+
         user_centered = screen_center_x, screen_center_y
 
         self.camera.move_to(user_centered, panning_fraction)
